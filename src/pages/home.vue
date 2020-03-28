0134efbcb5a74dd34b5b1024f1f29ed060681c6f
yoelnacho <nacho.test.developer@gmail.com>
2020-03-28 18:15:30 -0300

    new assets

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -5,11 +5,11 @@
       <f7-nav-left>
         <f7-link icon-ios="f7:menu" icon-aurora="f7:menu" icon-md="material:menu" panel-open="left"></f7-link>
       </f7-nav-left>
-      <f7-nav-title sliding>Daily</f7-nav-title>
+      <f7-nav-title sliding>Daily <span class="text-color-primary">7</span></f7-nav-title>
       <f7-nav-right>
         <f7-link icon-ios="f7:menu" icon-aurora="f7:menu" icon-md="material:menu" panel-open="right"></f7-link>
       </f7-nav-right>
-      <f7-nav-title-large sliding>Daily</f7-nav-title-large>
+      <f7-nav-title-large sliding>Daily <span class="text-color-primary">7</span></f7-nav-title-large>
     </f7-navbar>
     <!-- Toolbar-->
     <f7-toolbar bottom>
